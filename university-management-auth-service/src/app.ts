eb8a96b1a91844d0690d9b9291680375a13d966a
rezwanrahimr <rezwanrahim69@gmail.com>
2024-01-12 22:32:55 +0600

    setup eslint

--- university-management-auth-service/src/app.ts
+++ university-management-auth-service/src/app.ts
@@ -1,9 +1,14 @@
-import express from "express";
-const app = express()
+import express, { urlencoded } from "express";
+import cors from "cors";
+const app = express();
 
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 export default app;
