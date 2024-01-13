81f5243af3d6b96deac051f709bcba6cc8d31678
rezwanrahimr <rezwanrahim69@gmail.com>
2024-01-13 18:16:24 +0600

    set logger

--- university-management-auth-service/src/server.ts
+++ university-management-auth-service/src/server.ts
@@ -1,17 +1,22 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+import logger from './shared/logger'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("University Management Auth Service Database Connect Successfully");
-        app.listen(config.port, () => {
-            console.log(`University Management Auth Service listening on port ${config.port}`)
-        })
-    } catch (error) {
-        console.log("Database Connect Fail", error);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    logger.info(
+      'University Management Auth Service Database Connect Successfully',
+    )
+    app.listen(config.port, () => {
+      logger.info(
+        `University Management Auth Service listening on port ${config.port}`,
+      )
+    })
+  } catch (error) {
+    logger.error('Database Connect Fail', error)
+  }
 }
 
-main();
+main()
---
+++ university-management-auth-service/src/shared/logger.ts
@@ -0,0 +1,17 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'error',
+    format: winston.format.json(),
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'info' }),
+        new winston.transports.File({ filename: 'combined.log', level: 'error' }),
+    ],
+});
+
+export default logger;
