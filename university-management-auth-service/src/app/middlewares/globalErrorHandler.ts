82a6e1add7caa93cb78ff07164a7a6fdb286127d
rezwanrahimr <rezwanrahim69@gmail.com>
2024-01-20 18:12:32 +0600

    added validatiion error handler

--- university-management-auth-service/src/app/middlewares/globalErrorHandler.ts
+++ university-management-auth-service/src/app/middlewares/globalErrorHandler.ts
@@ -1,7 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-const globalErrorHandler = (err: string, req: Request, res: Response, next: NextFunction) => {
+import config from "../../config";
+import { IGenericErrorMessage } from "../../interfaces/error";
+const globalErrorHandler = (err, req: Request, res: Response, next: NextFunction) => {
     res.status(400).json({ error: err });
     next();
+
+    let statusCode = 500;
+    let message;
+    const errorMessage: IGenericErrorMessage[] = [];
+
+    if (err.name === "ValidationError") {
+        const simplifiedError = handleValidationError(err);
+    }
+
+    res.status(statusCode).json({
+        success: false,
+        message,
+        errorMessage,
+        stack: config.env !== 'production' ? err?.stack : undefined
+    })
 }
 
 export default globalErrorHandler;
