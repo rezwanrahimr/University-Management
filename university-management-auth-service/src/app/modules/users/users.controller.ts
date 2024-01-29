93dc25466457b423bc547a2021a1d0d8399cd6e6
rezwanrahimr <rezwanrahim69@gmail.com>
2024-01-29 15:17:00 +0600

    handle zod error message

--- university-management-auth-service/src/app/modules/users/users.controller.ts
+++ university-management-auth-service/src/app/modules/users/users.controller.ts
@@ -1,24 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import userService from './users.service'
-import { z } from "zod";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
 
   try {
-    // 
-    const createUserZodSchema = z.object({
-      body: z.object({
-        user: z.object({
-          role: z.string({
-            required_error: 'Role is Required'
-          }),
-          password: z.string().optional()
-        })
-      })
-    })
-
-    await createUserZodSchema.parseAsync(req);
     const { user } = req.body
     const result = await userService.createUser(user)
     res.status(200).json({
